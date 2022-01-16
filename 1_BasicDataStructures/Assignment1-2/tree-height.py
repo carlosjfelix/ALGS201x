3dcfad3b34fce51f7ac994a8c0d754eab86239c3
Carlos J Felix <felixcarlosj@gmail.com>
2022-01-15 23:28:13 -0500

    Saving progress on Assignment 1-2

--- 1_BasicDataStructures/Assignment1-2/tree-height.py
+++ 1_BasicDataStructures/Assignment1-2/tree-height.py
@@ -1,13 +1,24 @@
 # python3
 
 import sys, threading
+from pathlib import Path
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
+# creating class methods to calculate tree height
 class TreeHeight:
         def read(self):
-                self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
+                """TreeHeight method to read a file and create tree"""
+                # open directory where file is located and then read line by line
+                folder = "C:/Users/cjfel/OneDrive/Desktop/Programming/Python/Data Structures Fundamentals/1_BasicDataStructures/Assignment1-2/tests/"
+                textfile = folder + input("Input the name of the text file: ")
+                f = open(textfile)
+
+                # number of nodes n
+                self.n = int(f.readline())
+                
+                # reads second line of text file that contains parents of nodes
+                self.parent = list(map(int, f.readline().split()))
 
         def compute_height(self):
                 # Replace this code with a faster implementation
@@ -20,6 +31,7 @@ class TreeHeight:
                                 i = self.parent[i]
                         maxHeight = max(maxHeight, height);
                 return maxHeight;
+
 
 def main():
   tree = TreeHeight()
